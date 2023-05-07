08bb1c12e1956f97fbd05a2255aad0e14edc9b15
jainamthakkar <jainamthakkar111@gmail.com>
2023-05-07 23:28:47 +0530

    commit

--- fromInterfaces/InterfaceIfTest.java
+++ fromInterfaces/InterfaceIfTest.java
@@ -1,5 +1,7 @@
 package fromInterfaces;
 
+// dynamic stack as the need of stack's size is higher it will create another stack of 2* size
+
 interface IntStack{
 	void push(int item);
 	int pop();
@@ -20,7 +22,14 @@ class FixedStack implements IntStack{
 	public void push(int value) {
 		
 		if (tos == stck.length - 1) {
-			System.out.println("stack is full");
+			int tmp[] = new int[stck.length*2];
+			
+			for (int i = 0; i < stck.length; i++) 			
+				tmp[i] = stck[i];			
+			stck = tmp;
+			stck[++tos] = value;
+			
+//			System.out.println("stack is full");
 		}else {
 			stck[++tos] = value;			
 		}
@@ -48,17 +57,19 @@ public class InterfaceIfTest {
 		FixedStack fs2 = new FixedStack(8);
 		
 		//push
-		for (int i = 0; i < 5; i++) 
+		for (int i = 0; i < 7; i++) 
 			fs1.push(i);
-		for (int i = 0; i < 8; i++) 
+		for (int i = 0; i < 15; i++) 
 			fs2.push(i+1);
 		
+		
+		//pop
 		System.out.println("Stack 1 :- ");
-		for(int i = 0; i < 5; i++)
+		for(int i = 0; i < 7; i++)
 			System.out.println(fs1.pop());
 		
 		System.out.println("Stack 2 :- ");
-		for(int i = 0; i < 8; i++)
+		for(int i = 0; i < 15; i++)
 			System.out.println(fs2.pop());
 	}
 
