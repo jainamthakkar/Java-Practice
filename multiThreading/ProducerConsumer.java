c3550b7bc3a7be19d7103feeb0e1845cbe1b7ab8
jainamthakkar <jainamthakkar111@gmail.com>
2023-05-18 14:35:46 +0530

    threading files

--- multiThreading/ProducerConsumer.java
+++ multiThreading/ProducerConsumer.java
@@ -1,5 +1,5 @@
 package multiThreading;
-
+/*
 class Q {
 	int n;
 	boolean value = false;
@@ -84,5 +84,79 @@ public class ProducerConsumer {
 		c.t.start();
 
 		System.out.println("Press controll-C stop");
+	}
+}
+*/
+
+class Q {
+	int n;
+	boolean valueset = false;
+
+	synchronized int get() {
+		while (!valueset)
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("Thread Interrupted");
+			}
+		System.out.println("Got : " + n);
+		valueset = false;
+		notify();
+		return n;
+	}
+
+	synchronized void put(int n) {
+//		if (n < 10 && n > -1) {
+			while (valueset)
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					System.out.println("Thread interrupted");
+				}
+		this.n = n;
+		valueset = true;
+		System.out.println("put : " + n);
+		notify();
+//		}
+	}
+}
+
+class Producer implements Runnable {
+	Q q;
+
+	Producer(Q q) {
+		this.q = q;
+		new Thread(this, "Producer").start();
+	}
+
+	public void run() {
+		int i = 0;
+		while (true) {
+			q.put(i++);
+		}
+	}
+}
+
+class Consumer implements Runnable {
+	Q q;
+
+	Consumer(Q q) {
+		this.q = q;
+		new Thread(this, "Consumer").start();
+	}
+
+	public void run() {
+		while (true) {
+			q.get();
+		}
+	}
+}
+
+class ProducerConsumer {
+	public static void main(String args[]) {
+		Q q = new Q();
+		new Producer(q);
+		new Consumer(q);
+		System.out.println("press ctrl+c to exit");
 	}
 }
